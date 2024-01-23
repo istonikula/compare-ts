7b49bfec5797bbd7ef75697f5527a573711d099d
Isto Nikula <isto.nikula@iki.fi>
2024-01-23 20:16:18 +0200

    compare

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,104 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
+import { shuffle } from '@vitest/utils'
 
 import * as C from '.'
 
-test('sort number', () => {
-  expect([0, 2, 1, 4, 3, 6, 5, 8, 7].sort(C.compareNumber)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8])
+describe('sort numbers', () => {
+  const nums = shuffle(shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+  test('asc', () => {
+    expect(shuffle([...nums]).sort(C.compareNumber)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+  })
+  
+  test('desc', () => {
+    expect(shuffle([...nums]).sort(C.reversed(C.compareNumber))).toEqual([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+  })
+})
+
+describe('sort points', () => {
+  type Point = {
+    x: number
+    y: number
+  }
+  const points: Point[] = [
+    {x: 0, y: 0},
+    {x: 0, y: 1},
+    {x: 0, y: 2},
+    {x: 1, y: 0},
+    {x: 1, y: 1},
+    {x: 1, y: 2},
+  ]
+  const cmpX = C.compareBy<Point, number>(p => p.x, C.compareNumber)
+  const cmpY = C.compareBy<Point, number>(p => p.y, C.compareNumber)
+  
+  test('by single prop', () => {
+    const byY = [
+      {x: 0, y: 0},
+      {x: 1, y: 0},
+      {x: 0, y: 1},
+      {x: 1, y: 1},
+      {x: 0, y: 2},
+      {x: 1, y: 2},
+    ]
+  
+    expect([...points].sort(cmpY)).toEqual(byY)
+    expect([...byY].sort(cmpX)).toEqual(points)
+  })
+  
+  test('by y then by x desc', () => {
+    const { compare } = C.Chain.of(cmpY).thenBy(p => p.x, C.reversed(C.compareNumber))
+    expect(shuffle([...points]).sort(compare)).toEqual([
+      {x: 1, y: 0},
+      {x: 0, y: 0},
+      {x: 1, y: 1},
+      {x: 0, y: 1},
+      {x: 1, y: 2},
+      {x: 0, y: 2},
+    ])
+  })
+})
+
+test('nils last', () => {
+  expect([0, 2, undefined, 1, undefined, 3].sort(C.nilLast(C.compareNumber))).toEqual([0, 1, 2, 3, undefined, undefined])
+  expect([0, 2, null, 1, null, 3].sort(C.nilLast(C.compareNumber))).toEqual([0, 1, 2, 3, null, null])
+})
+
+describe('syntax example', () => {
+  type User = {
+    name: string
+    age?: number
+  }
+
+  const users: User[] = [
+    { name: 'John' },
+    { name: 'John', age: 25 },
+    { name: 'Bob'}
+  ]
+
+  const compareString: C.Compare<string> = (a, b) => (a < b ? -1 : a > b ? 1 : 0)
+
+  test('using by chain', () => {
+    const compareUser = C.Chain
+      .by<User, string>(x => x.name, compareString)
+      .thenBy(x => x.age, C.nilLast(C.compareNumber))
+      .compare
+
+    expect([...users].sort(compareUser)).toEqual([
+      { name: 'Bob'},
+      { name: 'John', age: 25 },
+      { name: 'John' },
+    ])
+  })
+
+  test('using compare chain', () => {
+    const compareName: C.Compare<User> = C.compareBy(x => x.name, compareString)
+    const compareAge: C.Compare<User> = C.compareBy(x => x.age, C.nilLast(C.compareNumber))
+    const compareUser = C.Chain.of(compareName).then(compareAge).compare
+
+    expect([...users].sort(compareUser)).toEqual([
+      { name: 'Bob'},
+      { name: 'John', age: 25 },
+      { name: 'John' },
+    ])
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,45 @@ export function reversed<T>(compare: Compare<T>): Compare<T> {
   return (a, b) => compare(b, a)
 }
 
+export function nilLast<T>(compare: Compare<T>): Compare<T | undefined | null> {
+  return (a, b) => {
+    if (!isNil(a) && !isNil(b)) return compare(a, b)
+    if (!isNil(a)) return -1
+    if (!isNil(b)) return 1
+    return 0
+  }
+}
+function isNil(value: unknown): value is null | undefined {
+  return value == undefined
+}
+
+export class Chain<T> {
+  readonly compare: Compare<T>
+
+  private constructor(compare: Compare<T>) {
+    this.compare = compare
+  }
+
+  static of<T>(compare: Compare<T>): Chain<T> {
+    return new Chain(compare)
+  }
+
+  static by<T, U>(selector: (t: T) => U, compareU: Compare<U>): Chain<T> {
+    return this.of(compareBy(selector, compareU))
+  }
+
+  then(next: Compare<T>): Chain<T> {
+    const curr = this.compare
+    return new Chain((a, b) => {
+      // apply next only if result remains undecided, i.e. 0
+      const result = curr(a, b)
+      return result !== 0 ? result : next(a, b)
+    })
+  }
+
+  thenBy<U>(selector: (t: T) => U, compareU: Compare<U>): Chain<T> {
+    return this.then(compareBy(selector, compareU))
+  }
+}
+
 export const compareNumber: Compare<number> = (a, b) => (a < b ? -1 : a > b ? 1 : 0)
